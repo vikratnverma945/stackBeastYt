9df9d22fd6965b284280656a7870f22654a5bb88
VIKRANT VERMA <vikrantverma945@gmail.com>
2021-09-20 21:55:24 +0530

    added new Content

--- script_3.js
+++ script_3.js
@@ -1,20 +1,4 @@
-// function printTable(number) {
-//   i = 1;
-//   while (i <= 10) {
-//     console.log(number + "*" + i + "=" + i * number);
-//     i = i + 1;
-//   }
-// }
+let number = 5;
+let total = (number / 2) * (2 + (number - 1) * 1);
 
-// printTable(10);
-
-function addNumber(num1 = 63, num2 = 5) {
-  return num1 + num2;
-}
-
-let total = addNumber(20, 5) + 10;
-console.log("Tota", total);
-
-console.log("Tota", addNumber(20 + 5, 20) + addNumber(10, 10));
-
-console.log("Total", 40 + 5, 78 + 2);
+console.log("Total", total);
